9a8d730375ba13fd58d6f0aaf77a04fd74758f95
hadyRashwan <h2rashwan@gmail.com>
2019-03-24 10:08:38 +0200

    inital working converter

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
 const XLSX = require("xlsx");
 const fs = require('fs');
 
+// constants
+const languageDirectory = 'languages'
+const excelFilePath='./excel.xlsx'
 
+//create array of jsons from excel file
 function convertExcelToJson(path) {
   const workbook = XLSX.readFile(path);
   const sheet_name_list = workbook.SheetNames;
   const rows = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
   return rows;
 }
+
+// create big json from excel rows 
 const translationMap = {};
-const rows = convertExcelToJson("./excel.xlsx");
+const rows = convertExcelToJson(excelFilePath);
 for (const row of rows) {
+  // console.log( Object.keys(row))
+
   for (const header of Object.keys(row)) {
-    const { key } = row;
-    translationMap[header] =
-      header != "key"
-        ? { ...translationMap[header], ...row[key] }
-        : translationMap[header];
+    const key  = row.Key;
+    if(header != 'Key'){
+      const object = typeof translationMap[header] == 'undefined'?  {}  : translationMap[header]
+      object[key] = row[header] 
+      translationMap[header] = object
+    }
   }
 }
-for(language of Object.keys(translationMap) ){
-    fs.writeFile(`languages/${language}.json`, json, 'utf8', function(err) {
-        if (err) throw err;
-        console.log(` ${language} is complete`);
-        })
 
+
+// create folder if it doesnt exist
+if (!fs.existsSync(languageDirectory)){
+  fs.mkdirSync(languageDirectory);
+}
+// create files
+for(language of Object.keys(translationMap) ){
+  console.log(`writing ${language}`)
+    fs.writeFile(`${languageDirectory}/${language}.json`, JSON.stringify(translationMap[language]), 'utf8', function(err) {
+        if (err) throw err;
+        console.log(`language is complete`);
+        })
 }
 
